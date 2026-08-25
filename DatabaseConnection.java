2462937016d318d61c985f1f828244d5bd2d5fa3
Dan Christian <danchristianniyomugabo@gmail.com>
2026-08-25 15:23:23 +0200

    Addition of DB user

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -4,4 +4,6 @@ import java.sql.SQLException;
 
 public class DatabaseConnection{
     private static final String URL = "jdbc:postgresql://localhost:5432/library_db";
+    private static final String USER = "postgres";
+
 }
