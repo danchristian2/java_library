bc0f05b97b72e79a0f5010a20999f2526615f708
Dan Christian <danchristianniyomugabo@gmail.com>
2026-08-25 15:25:54 +0200

    Addition a function

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -7,5 +7,8 @@ public class DatabaseConnection{
     private static final String USER = "postgres";
     private static final String PASSWORD = "your password";
 
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 
 }
