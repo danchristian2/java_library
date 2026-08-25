2e2e62cc5031992fbe964a4decc7dfb3d48a01dc
Dan Christian <danchristianniyomugabo@gmail.com>
2026-08-25 15:21:53 +0200

    Addition of DB string

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -3,5 +3,5 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection{
-
+    private static final String URL = "jdbc:postgresql://localhost:5432/library_db";
 }
