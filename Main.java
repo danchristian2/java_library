b1063d7d7af621fc3f4273d26c9d5f5543e809ce
Dan Christian <danchristianniyomugabo@gmail.com>
2026-08-25 15:28:05 +0200

    Addition of codes

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello");
-        System.out.println("Dan");
+import java.sql.Connection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+
+            System.out.println("Connected to PostgreSQL!");
+
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
